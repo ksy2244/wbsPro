0218fd64335bd03e76b3da88e7257d1f9eb8afe7
ksy2244 <rlathdus2244@naver.com>
2022-08-26 21:45:42 +0900

    EmployeeDAO, EmployeeDAOImpl, EmployeeDTO, EmployeeUI, MainUI, PlanUI 추가

--- wbsPro/src/main/java/com/main/App.java
+++ wbsPro/src/main/java/com/main/App.java
@@ -5,7 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("WBS 일정 관리 프로그램");
-
+		MainUI ui = new MainUI();
+		ui.menu();
 	}
 
 }
