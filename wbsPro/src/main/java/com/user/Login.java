5311054b35ea75edb74ced0c2243ec6d6910705a
ksy2244 <rlathdus2244@naver.com>
2022-08-28 22:26:52 +0900

    사원 코드로 로그인, 비밀번호 변

---
+++ wbsPro/src/main/java/com/user/Login.java
@@ -0,0 +1,19 @@
+package com.user;
+
+import com.emp.EmployeeDTO;
+
+public class Login {
+	private EmployeeDTO loginEmployee = null;
+
+	public EmployeeDTO loginEmployee() {
+		return loginEmployee;
+	}
+
+	public void login(EmployeeDTO loginEmployee) {
+		this.loginEmployee = loginEmployee;
+	}
+
+	public void logout() {
+		loginEmployee = null;
+	}
+}
