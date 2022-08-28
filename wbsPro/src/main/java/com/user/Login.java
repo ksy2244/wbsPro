6a3523cbeabf602eacc6414dd4244f4040341d52
ksy2244 <rlathdus2244@naver.com>
2022-08-28 21:25:26 +0900

    사원 번호로 로그인, 비밀번호 변경

--- wbsPro/src/main/java/com/user/Login.java
+++ wbsPro/src/main/java/com/user/Login.java
@@ -4,15 +4,15 @@ import com.emp.EmployeeDTO;
 
 public class Login {
 	private EmployeeDTO loginEmployee = null;
-	
+
 	public EmployeeDTO loginEmployee() {
 		return loginEmployee;
 	}
-	
+
 	public void login(EmployeeDTO loginEmployee) {
 		this.loginEmployee = loginEmployee;
 	}
-	
+
 	public void logout() {
 		loginEmployee = null;
 	}
