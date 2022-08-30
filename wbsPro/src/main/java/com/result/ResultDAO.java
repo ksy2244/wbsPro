ce437d5279e22224f0e6bc68069597b7193df526
User <User@KaNRGH>
2022-08-30 17:39:16 +0900

    ResultDAO 추가

--- wbsPro/src/main/java/com/result/ResultDAO.java
+++ wbsPro/src/main/java/com/result/ResultDAO.java
@@ -2,6 +2,10 @@ package com.result;
 
 import java.sql.SQLException;
 
+import com.subject.SubjectDTO;
+import com.cat.CatDateDTO;
+import com.op.OpDateDTO;
+
 public interface ResultDAO { // 실적 관리
 	
 	
@@ -11,9 +15,23 @@ public interface ResultDAO { // 실적 관리
 	
 	
 	
-	public int workCompositionOpDate(int codeCatDate, int codeOpDate, int comp) throws SQLException; // 소분류
-	public int workCompositionCatDate(int codeSubDate, int codeCatDate, int comp) throws SQLException; // 중분류
-	public int workCompositionSubDate(int codeProject, int codeSubDate, int comp) throws SQLException; // 대분류
+	// 대분류실적시작일시작
+		public int ResultProgressSubDateStartInput(SubjectDTO dto) throws SQLException;
+		
+		// 대분류실적종료일시작
+		public int ResultProgressSubDateEndInput(SubjectDTO dto) throws SQLException;
+		
+		// 중분류실적시작일시작
+		public int ResultProgressCatDateStartInput(CatDateDTO dto) throws SQLException;
+			
+		// 중분류실적종료일시작
+		public int ResultProgressCatDateEndInput(CatDateDTO dto) throws SQLException;
+			
+		// 소분류실적시작일시작
+		public int ResultProgressOpDateStartInput(OpDateDTO dto) throws SQLException;
+			
+		// 소분류실적종료일시작
+		public int ResultProgressOpDateEndInput(OpDateDTO dto) throws SQLException;
 	
 	
 }
