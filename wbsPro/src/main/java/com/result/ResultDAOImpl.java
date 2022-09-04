f2494ec1814ab2dd8d0adcf0d82d0e1b80fdd88c
ksy2244 <rlathdus2244@naver.com>
2022-09-04 09:10:37 +0900

    소분류 실적 시작일 오류 수정

--- wbsPro/src/main/java/com/result/ResultDAOImpl.java
+++ wbsPro/src/main/java/com/result/ResultDAOImpl.java
@@ -14,105 +14,96 @@ import com.util.DBConn;
 
 public class ResultDAOImpl implements ResultDAO {
 	private Connection conn = DBConn.getConnection();
-	
-	
 
 	@Override
-	public ProjectDTO projectbetweenDate(int prj_code) {  // 해당 프로젝트코드에 맞는 실적 시작일과 종료일 반환
+	public ProjectDTO projectbetweenDate(int prj_code) { // 해당 프로젝트코드에 맞는 실적 시작일과 종료일 반환
 		ProjectDTO dto = new ProjectDTO();
 		PreparedStatement pstmt = null;
 		String sql;
 		ResultSet rs = null;
-		
+
 		try {
-			
+
 			sql = "SELECT TO_CHAR(PRJ_START, 'YYYY-MM-DD') PRJ_START, TO_CHAR(PRJ_END, 'YYYY-MM-DD') PRJ_END FROM project WHERE prj_code = ?";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setInt(1, prj_code);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				dto.setPrj_start(rs.getString("PRJ_START"));
 				dto.setPrj_end(rs.getString("PRJ_END"));
-			} 
-			
-			
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
 		}
-		
+
 		return dto;
 	}
-	
-	
+
 	@Override
-	public String ProgressStartSubDate(int subDateCode) throws SQLException{
-		
+	public String ProgressStartSubDate(int subDateCode) throws SQLException {
+
 		PreparedStatement pstmt = null;
 		String sql;
 		ResultSet rs = null;
 		String subStart = "";
-		
+
 		try {
-			
+
 			sql = " SELECT TO_CHAR(SUB_START, 'YYYY-MM-DD') SUB_START FROM SUBDATE WHERE SUB_DATE_CODE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setInt(1, subDateCode);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				subStart = rs.getString("SUB_START");
-			} 
-			
-			
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
 		}
-		
+
 		return subStart;
 	}
-
-
-
-
 
 	@Override
 	public String ProgressStartCatDate(int catDateCode) throws SQLException {
@@ -120,48 +111,43 @@ public class ResultDAOImpl implements ResultDAO {
 		String sql;
 		ResultSet rs = null;
 		String catStart = "";
-		
+
 		try {
-			
+
 			sql = " SELECT TO_CHAR(CAT_START, 'YYYY-MM-DD') CAT_START FROM CATDATE WHERE CAT_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setInt(1, catDateCode);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				catStart = rs.getString("CAT_START");
-			} 
-			
-			
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
 		}
-		
+
 		return catStart;
 	}
-
-
-
-
 
 	@Override
 	public String ProgressStartOpDate(int opDateCode) throws SQLException {
@@ -169,82 +155,78 @@ public class ResultDAOImpl implements ResultDAO {
 		String sql;
 		ResultSet rs = null;
 		String opStart = "";
-		
+
 		try {
-			
+
 			sql = " SELECT TO_CHAR(OP_START, 'YYYY-MM-DD') OP_START FROM OPDATE WHERE OP_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setInt(1, opDateCode);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				opStart = rs.getString("OP_START");
-			} 
-			
-			
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e2) {
-					
+
 				}
 			}
 		}
-		
+
 		return opStart;
 	}
 
-
-
-	@Override  // 소분류 실적 진척율 입력
+	@Override // 소분류 실적 진척율 입력
 	public int perforProgressOpDateUpdate(int cat_date, int op_date, int performOpDate) throws SQLException {
-		
-		int result = 0, output= 0;
+
+		int result = 0, output = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql;
-		
+
 		try {
-			
+
 			sql = " UPDATE OPDATE SET OP_PER = ? WHERE OP_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, performOpDate);
 			pstmt.setInt(2, op_date);
 			pstmt.executeUpdate();
-			
+
 			pstmt.close();
 			pstmt = null;
-			
-			
+
 			// 합 100 초과했는지 확인
-			
+
 			sql = " SELECT SUM(OP_PER) OP_PER FROM OPDATE WHERE CAT_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, cat_date);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				output = rs.getInt("OP_PER");
 			}
-			
-			if(output != 100) {
+
+			if (output != 100) {
 				conn.rollback();
 			} else {
 				result = 1;
@@ -252,58 +234,51 @@ public class ResultDAOImpl implements ResultDAO {
 				conn.setAutoCommit(true);
 
 			}
-			
-			
-		
+
 		} catch (SQLException e) {
-			
+
 			try {
 				conn.rollback();
 			} catch (Exception e2) {
-				
+
 			}
-		}  finally {
-			
-			if(rs != null) {
+		} finally {
+
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
+
 			try {
 				// conn.setAutoCommit(true);
 			} catch (Exception e) {
-				
+
 			}
-			
+
 		}
-		
+
 		return result;
-		
+
 	}
 
-
-
-	
-	
-	@Override  // 프로젝트 실적시작일
+	@Override // 프로젝트 실적시작일
 	public int resultProgressProjectStartInput(ProjectDTO dto) throws SQLException {
-		int result = 0; 
+		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql;
-		
-		
-		try{
+
+		try {
 			// 프로젝트일정 수정 sql
 			sql = "UPDATE project SET prj_start = ? WHERE prj_code = ?";
 
@@ -311,7 +286,7 @@ public class ResultDAOImpl implements ResultDAO {
 
 			pstmt.setString(1, dto.getPrj_start()); // 프로젝트실적시작일
 			pstmt.setInt(2, dto.getPrj_code());// 프로젝트코드
-			
+
 			result = pstmt.executeUpdate();
 
 			System.out.println("프로젝트실적시작일 추가");
@@ -320,30 +295,26 @@ public class ResultDAOImpl implements ResultDAO {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
 		}
-		
+
 		return result;
 	}
 
-
-
-	@Override  // 프로젝트 실적종료일
+	@Override // 프로젝트 실적종료일
 	public int resultProgressProjectEndInput(ProjectDTO dto) throws SQLException {
-		int result = 0; 
+		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql;
-		
-		
-		try{
+
+		try {
 			// 프로젝트일정 수정 sql
 			sql = "UPDATE project SET prj_end = ? WHERE prj_code = ?";
 
@@ -351,7 +322,7 @@ public class ResultDAOImpl implements ResultDAO {
 
 			pstmt.setString(1, dto.getPrj_end()); // 프로젝트실적종료일
 			pstmt.setInt(2, dto.getPrj_code());// 프로젝트코드
-			
+
 			result = pstmt.executeUpdate();
 
 			System.out.println("프로젝트실적종료일 추가");
@@ -360,64 +331,52 @@ public class ResultDAOImpl implements ResultDAO {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
 		}
 		return result;
 	}
-
-	
-	
-
-	
 
 	@Override // 대분류 실적시작일
 	public int resultProgressSubDateStartInput(SubjectDTO dto) throws SQLException {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql;
-		
+
 		try {
-	
-			sql = "UPDATE SUBDATE SET SUB_START = ? "
-				+ "WHERE SUB_DATE_CODE = ? ";
+
+			sql = "UPDATE SUBDATE SET SUB_START = ? " + "WHERE SUB_DATE_CODE = ? ";
 
 			pstmt = conn.prepareStatement(sql);
 
-			pstmt.setString(1, dto.getSub_start()); 
+			pstmt.setString(1, dto.getSub_start());
 			pstmt.setInt(2, dto.getSub_date_code());
 			result = pstmt.executeUpdate();
-
-			
-			
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
 		}
 		return result;
 	}
 
-
-	
 	@Override // 대분류 실적종료일시작
-	public int resultProgressSubDateEndInput(SubjectDTO dto) throws SQLException{
+	public int resultProgressSubDateEndInput(SubjectDTO dto) throws SQLException {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql;
@@ -430,19 +389,19 @@ public class ResultDAOImpl implements ResultDAO {
 			pstmt.setString(1, dto.getSub_end()); // 대분류실적종료일
 			pstmt.setInt(2, dto.getSub_date_code()); // 대분류 코드
 			result = pstmt.executeUpdate();
-			
+
 			System.out.println("대분류실적종료일 추가");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
 		}
@@ -463,32 +422,31 @@ public class ResultDAOImpl implements ResultDAO {
 			pstmt.setString(1, dto.getCat_start()); // 중분류실적시작일
 			pstmt.setInt(2, dto.getCat_date()); // 중분류 코드
 			result = pstmt.executeUpdate();
-			
+
 			System.out.println("중분류실적시작일 추가");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
 		}
 		return result;
 	}
 
-	
 	@Override // 중분류실적종료일시작
-	public int resultProgressCatDateEndInput(CatDateDTO dto) throws SQLException{
+	public int resultProgressCatDateEndInput(CatDateDTO dto) throws SQLException {
 		int result = 0;
 		PreparedStatement pstmt = null;
 		String sql;
-		
+
 		try {
 			// 중분류일정 수정 sql
 			sql = "UPDATE catdate SET cat_end = ? WHERE cat_date = ? AND cat_start is not null";
@@ -498,30 +456,31 @@ public class ResultDAOImpl implements ResultDAO {
 			pstmt.setString(1, dto.getCat_end()); // 중분류실적종료일
 			pstmt.setInt(2, dto.getCat_date()); // 중분류 코드
 			result = pstmt.executeUpdate();
-			
+
 			System.out.println("중분류실적종료일 추가");
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
 		}
 		return result;
 	}
-	
-	@Override // 소분류실적시작일 
+
+	@Override // 소분류실적시작일
 	public int resultProgressOpDateStartInput(OpDateDTO dto) throws SQLException {
 		PreparedStatement pstmt = null;
 		String sql;
-		
+		int result = 0;
+
 		try {
 			// 소분류일정 수정 sql
 			sql = "UPDATE opdate SET op_start = ? WHERE op_date = ?";
@@ -530,25 +489,23 @@ public class ResultDAOImpl implements ResultDAO {
 
 			pstmt.setString(1, dto.getOp_start()); // 소분류실적시작일
 			pstmt.setInt(2, dto.getOp_date()); // 소분류 코드
-			pstmt.executeUpdate();
-			
+			result = pstmt.executeUpdate();
+
 			System.out.println("소분류실적시작일 추가");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return 0;
+		return result;
 	}
 
-
-	
 	@Override // 소분류실적종료일
-	public int resultProgressOpDateEndInput(OpDateDTO dto) throws SQLException{
+	public int resultProgressOpDateEndInput(OpDateDTO dto) throws SQLException {
 		PreparedStatement pstmt = null;
 		String sql;
-		
+
 		try {
-			//소분류일정 수정 sql
+			// 소분류일정 수정 sql
 			sql = "UPDATE opdate SET op_end = ? WHERE op_date = ? AND op_start is not null";
 
 			pstmt = conn.prepareStatement(sql);
@@ -557,53 +514,51 @@ public class ResultDAOImpl implements ResultDAO {
 			pstmt.setInt(2, dto.getOp_date()); // 소분류 코드
 			pstmt.executeUpdate();
 			pstmt.close();
-			
+
 			System.out.println("소분류실적종료일 추가");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return 0;
 	}
-	
 
 	@Override
-	public int workCompositionOpDate(int codeCatDate ,int codeOpDate, int comp) throws SQLException {
-		
+	public int workCompositionOpDate(int codeCatDate, int codeOpDate, int comp) throws SQLException {
+
 		int output = 0;
 		PreparedStatement pstmt = null;
 		String sql;
 		ResultSet rs = null;
-		
+
 		try {
 			conn.setAutoCommit(false);
-			
+
 			sql = " UPDATE OPDATE SET WORK_COMP = ? WHERE OP_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, comp);
 			pstmt.setInt(2, codeOpDate);
 			pstmt.executeUpdate();
-			
+
 			pstmt.close();
 			pstmt = null;
-			
+
 			// 합 100 초과했는지 확인
-			
+
 			// sql = " SELECT WORK_COMP FROM OPDATE WHERE CAT_DATE = ? ";
 			sql = " SELECT SUM(WORK_COMP) WORK_COMP FROM OPDATE WHERE CAT_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, codeCatDate);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				output = rs.getInt("WORK_COMP");
 			}
-		
-			
-			if(output != 100) {
+
+			if (output != 100) {
 				conn.rollback();
 				return output;
 			} else {
@@ -611,83 +566,79 @@ public class ResultDAOImpl implements ResultDAO {
 				conn.setAutoCommit(true);
 
 			}
-			
-			
-		
+
 		} catch (SQLException e) {
-			
+
 			try {
 				conn.rollback();
 			} catch (Exception e2) {
-				
+
 			}
 			System.out.println("업무구성비 입력 실패");
-		}  finally {
-			
-			if(rs != null) {
+		} finally {
+
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
+
 			try {
 				// conn.setAutoCommit(true);
 			} catch (Exception e) {
-				
+
 			}
-			
-			
-			
+
 		}
-		
+
 		return output;
 	}
 
 	@Override
 	public int workCompositionCatDate(int codeSubDate, int codeCatDate, int comp) throws SQLException {
-		
+
 		int output = 0;
 		PreparedStatement pstmt = null;
 		String sql;
 		ResultSet rs = null;
-		
+
 		try {
 			conn.setAutoCommit(false);
-			
+
 			sql = " UPDATE CATDATE SET CAT_COMP = ?  WHERE CAT_DATE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, comp);
 			pstmt.setInt(2, codeCatDate);
 			pstmt.executeUpdate();
-			
+
 			pstmt.close();
 			pstmt = null;
-			
+
 			// 합 100 초과했는지 확인
-			
+
 			sql = " SELECT SUM(CAT_COMP) CAT_COMP FROM CATDATE WHERE SUB_DATE_CODE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, codeSubDate);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				output = rs.getInt("CAT_COMP");
 			}
-			
-			if(output != 100) {
+
+			if (output != 100) {
 				conn.rollback();
 				return output;
 			} else {
@@ -695,84 +646,80 @@ public class ResultDAOImpl implements ResultDAO {
 				conn.setAutoCommit(true);
 
 			}
-			
-			
-		
+
 		} catch (SQLException e) {
-			
+
 			try {
 				conn.rollback();
 			} catch (Exception e2) {
-				
+
 			}
 			System.out.println("업무구성비 입력 실패");
-		}  finally {
-			
-			if(rs != null) {
+		} finally {
+
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
+
 			try {
 				// conn.setAutoCommit(true);
 			} catch (Exception e) {
-				
+
 			}
-			
+
 		}
-		
+
 		return output;
-		
+
 	}
 
 	@Override
 	public int workCompositionSubDate(int codeProject, int codeSubDate, int comp) throws SQLException {
-		
+
 		int output = 0;
 		PreparedStatement pstmt = null;
 		String sql;
 		ResultSet rs = null;
-		
+
 		try {
 			conn.setAutoCommit(false);
-			
+
 			sql = " UPDATE SUBDATE SET SUB_COMP = ? WHERE SUB_DATE_CODE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, comp);
 			pstmt.setInt(2, codeSubDate);
 			pstmt.executeUpdate();
-			
+
 			pstmt.close();
 			pstmt = null;
-			
-			
-			
+
 			// 합 100 초과했는지 확인
-			
+
 			sql = " SELECT SUM(SUB_COMP) SUB_COMP FROM SUBDATE WHERE PRJ_CODE = ? ";
-			
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, codeProject);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				output = rs.getInt("SUB_COMP");
 			}
-			
-			if(output != 100) {
+
+			if (output != 100) {
 				conn.rollback();
 				return output;
 			} else {
@@ -780,46 +727,43 @@ public class ResultDAOImpl implements ResultDAO {
 				conn.setAutoCommit(true);
 
 			}
-			
-			
-		
+
 		} catch (SQLException e) {
-			
+
 			try {
 				conn.rollback();
 			} catch (Exception e2) {
-				
+
 			}
 			System.out.println("업무구성비 입력 실패");
-		}  finally {
-			
-			if(rs != null) {
+		} finally {
+
+			if (rs != null) {
 				try {
 					rs.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
-			if(pstmt != null) {
+
+			if (pstmt != null) {
 				try {
 					pstmt.close();
 				} catch (Exception e) {
-					
+
 				}
 			}
-			
+
 			try {
 				// conn.setAutoCommit(true);
 			} catch (Exception e) {
-				
-			}
-			
-		}
-		
-		return output;
-		
-	}
 
+			}
+
+		}
+
+		return output;
+
+	}
 
 }
--- wbsPro/src/main/java/com/result/ResultUI.java
+++ wbsPro/src/main/java/com/result/ResultUI.java
@@ -16,53 +16,64 @@ import com.util.DBConn;
 public class ResultUI {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	ResultDAO dao = new ResultDAOImpl();
-	
-	int ch ;
-	
-	public void menu() { 
-		
+
+	int ch;
+
+	public void menu() {
+
 		while (true) {
 
 			try {
 				System.out.print("1.업무구성비 입력 2.실적시작일 입력 3.실적종료일 입력 4.실적진척율 입력 5.뒤로가기");
 
-				 ch = Integer.parseInt(br.readLine());
-
+				ch = Integer.parseInt(br.readLine());
 
 				switch (ch) {
-				case 1: compositMenu(); break;
-				case 2: resultProgDateStart(); break;
-				case 3: resultProgDateEnd(); break;
-				case 4: resultProgressPerOpDate(); break; // 소분류 실적진척율 입력
-				case 5: return; 
+				case 1:
+					compositMenu();
+					break;
+				case 2:
+					resultProgDateStart();
+					break;
+				case 3:
+					resultProgDateEnd();
+					break;
+				case 4:
+					resultProgressPerOpDate();
+					break; // 소분류 실적진척율 입력
+				case 5:
+					return;
 				}
 
 			} catch (Exception e) {
 				e.printStackTrace();
-				
+
 			}
 
 		}
 	}
-		
 
-	
 	public void compositMenu() { // 업무구성비 메뉴
-		
+
 		while (true) {
 
 			try {
 				System.out.print("1.소분류업무구성비 입력 2.중분류업무구성비 입력 3.대분류업무구성비 입력 4.뒤로가기");
 
-				 ch = Integer.parseInt(br.readLine());
-
-
+				ch = Integer.parseInt(br.readLine());
 
 				switch (ch) {
-				case 1: compositionOpDate(); break;
-				case 2: compositionCatDate(); break;
-				case 3: compositionSubDate(); break;
-				case 4: return; 
+				case 1:
+					compositionOpDate();
+					break;
+				case 2:
+					compositionCatDate();
+					break;
+				case 3:
+					compositionSubDate();
+					break;
+				case 4:
+					return;
 				}
 
 			} catch (Exception e) {
@@ -71,211 +82,194 @@ public class ResultUI {
 
 		}
 	}
-	
+
 	public void resultProgDateStart() { // 실적시작일 입력
 		while (true) {
 
 			try {
 				System.out.print("1.프로젝트실적시작일 입력 2.대분류실적시작일 입력 3.중분류실적시작일 입력 4.소분류실적시작일 입력  5.뒤로가기");
 
-				 ch = Integer.parseInt(br.readLine());
-
-
+				ch = Integer.parseInt(br.readLine());
 
 				switch (ch) {
-				case 1: resultProgressProjectStartInput();break;
-				case 2: resultProgressSubDateStart();break;
-				case 3: resultProgressCatDateStartInput();break;
-				case 4: resultProgressOpDateStartInput();break;
-				case 5: return; 
+				case 1:
+					resultProgressProjectStartInput();
+					break;
+				case 2:
+					resultProgressSubDateStart();
+					break;
+				case 3:
+					resultProgressCatDateStartInput();
+					break;
+				case 4:
+					resultProgressOpDateStartInput();
+					break;
+				case 5:
+					return;
 				}
 
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		
+
 	}
-	
+
 	public void resultProgressProjectStartInput() {// 실적시작일 입력(프로젝트)
 		System.out.println("프로젝트 실적 시작일 입력");
-		
+
 		try {
 			ProjectDTO dto = new ProjectDTO();
-			
+
 			System.out.print("프로젝트 코드 ? ");
 			int n = Integer.parseInt(br.readLine());
-			dto.setPrj_code(n);	
-			
+			dto.setPrj_code(n);
+
 			System.out.print("프로젝트 실적 시작일을 입력하세요.");
-			dto.setPrj_start(br.readLine());	
-			
-			
+			dto.setPrj_start(br.readLine());
+
 			int result = dao.resultProgressProjectStartInput(dto);
-			
-			if(result != 0) {
-				 System.out.println("[프로젝트 실적시작일 입력 성공]");
-			 } else {
-				 System.out.println("[프로젝트 실적시작일 입력 실패]");
-			 }
-			
+
+			if (result != 0) {
+				System.out.println("[프로젝트 실적시작일 입력 성공]");
+			} else {
+				System.out.println("[프로젝트 실적시작일 입력 실패]");
+			}
+
 		} catch (Exception e) {
 			System.out.println("[프로젝트 실적시작일 입력 오류]");
 		}
-		
+
 	}
-	
-		
+
 	public void resultProgressSubDateStart() {// 실적시작일 입력(대분류)
 		System.out.println("대분류 실적 시작일 입력");
 		int result = 0;
-		
+
 		try {
 			SubjectDTO dto = new SubjectDTO();
-			
-			
+
 			System.out.print("프로젝트 코드 ? ");
 			int prjCode = Integer.parseInt(br.readLine());
-			
+
 			ProjectDTO pdto = dao.projectbetweenDate(prjCode);
-			
-			
+
 			System.out.print("대분류 코드 ? ");
 			int n = Integer.parseInt(br.readLine());
-			dto.setSub_date_code(n);	
-			
+			dto.setSub_date_code(n);
+
 			System.out.print("대분류 실적 시작일을 입력하세요.");
-			dto.setSub_start(br.readLine());	
-			
-			
-			
+			dto.setSub_start(br.readLine());
+
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			Date date1 = sdf.parse(pdto.getPrj_start());
 			Date date2 = sdf.parse(pdto.getPrj_end());
-			
+
 			Date datePro = sdf.parse(dto.getSub_start());
-			
-			
-			
-			if(datePro.compareTo(date1) >= 0 && // 입력 날짜 >= 시작일
-			   datePro.compareTo(date2) <= 0) { // 입력 날짜 <= 종료일
+
+			if (datePro.compareTo(date1) >= 0 && // 입력 날짜 >= 시작일
+					datePro.compareTo(date2) <= 0) { // 입력 날짜 <= 종료일
 				result = dao.resultProgressSubDateStartInput(dto);
 			}
-			
-			
-			
-			if(result != 0) {
-				 System.out.println("[대분류 실적시작일 입력 성공]");
-			 } else {
-				 System.out.println("[대분류 실적시작일 입력 실패]");
-			 }
-			
+
+			if (result != 0) {
+				System.out.println("[대분류 실적시작일 입력 성공]");
+			} else {
+				System.out.println("[대분류 실적시작일 입력 실패]");
+			}
+
 		} catch (Exception e) {
 			System.out.println("[대분류 실적시작일 입력 오류]");
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void resultProgressCatDateStartInput() {// 실적시작일 입력(중분류)
 		System.out.println("중분류 실적 시작일 입력");
-		
+
 		int result = 0;
-		
+
 		try {
-			
-			CatDateDTO dto = new CatDateDTO();			
-			
+
+			CatDateDTO dto = new CatDateDTO();
+
 			System.out.print("프로젝트 코드 ? ");
 			int prjCode = Integer.parseInt(br.readLine());
-			
+
 			ProjectDTO pdto = dao.projectbetweenDate(prjCode);
-			
-			
+
 			System.out.print("중분류 코드 ? ");
 			int n = Integer.parseInt(br.readLine());
-			dto.setCat_date(n);	
-			
+			dto.setCat_date(n);
+
 			System.out.print("중분류 실적 시작일을 입력하세요.");
 			dto.setCat_start(br.readLine());
-			
-			
-			
+
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			Date date1 = sdf.parse(pdto.getPrj_start());
 			Date date2 = sdf.parse(pdto.getPrj_end());
-			
+
 			Date datePro = sdf.parse(dto.getCat_start());
-			
-			
-			
-			if(datePro.compareTo(date1) >= 0 && // 입력 날짜 >= 시작일
-			   datePro.compareTo(date2) <= 0) { // 입력 날짜 <= 종료일
+
+			if (datePro.compareTo(date1) >= 0 && // 입력 날짜 >= 시작일
+					datePro.compareTo(date2) <= 0) { // 입력 날짜 <= 종료일
 				result = dao.resultProgressCatDateStartInput(dto);
-			} 
-			
-			
-			
-			if(result != 0) {
-				 System.out.println("[중분류 실적시작일 입력 성공]");
-			 } else {
-				 System.out.println("[중분류 실적시작일 입력 실패]");
-			 }
-			
+			}
+
+			if (result != 0) {
+				System.out.println("[중분류 실적시작일 입력 성공]");
+			} else {
+				System.out.println("[중분류 실적시작일 입력 실패]");
+			}
+
 		} catch (Exception e) {
 			System.out.println("[중분류 실적시작일 입력 오류]");
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void resultProgressOpDateStartInput() {// 실적시작일 입력(소분류)
 		System.out.println("소분류 실적 시작일 입력");
-		
+
 		int result = 0;
-		
+
 		try {
-			
-			OpDateDTO dto = new OpDateDTO();			
-			
+
+			OpDateDTO dto = new OpDateDTO();
+
 			System.out.print("프로젝트 코드 ? ");
 			int prjCode = Integer.parseInt(br.readLine());
-			
+
 			ProjectDTO pdto = dao.projectbetweenDate(prjCode);
-			
-			
+
 			System.out.print("소분류 코드 ? ");
 			int n = Integer.parseInt(br.readLine());
-			dto.setOp_date(n);	
-			
+			dto.setOp_date(n);
+
 			System.out.print("소분류 실적 시작일을 입력하세요.");
-			dto.setOp_plan_start(br.readLine());
-			
-			
-			
+			dto.setOp_start(br.readLine());
+
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			Date date1 = sdf.parse(pdto.getPrj_start());
 			Date date2 = sdf.parse(pdto.getPrj_end());
-			
+
 			Date datePro = sdf.parse(dto.getOp_start());
-			
-			
-			
-			if(datePro.compareTo(date1) >= 0 && // 입력 날짜 >= 시작일
-			   datePro.compareTo(date2) <= 0) { // 입력 날짜 <= 종료일
+
+			if (datePro.compareTo(date1) >= 0 && // 입력 날짜 >= 시작일
+					datePro.compareTo(date2) <= 0) { // 입력 날짜 <= 종료일
 				result = dao.resultProgressOpDateStartInput(dto);
-			} 
-			
-			
-			
-			if(result != 0) {
+			}
+
+			if (result != 0) {
 				 System.out.println("[소분류 실적시작일 입력 성공]");
 			 } else {
 				 System.out.println("[소분류 실적시작일 입력 실패]");
 			 }
 			
 		} catch (Exception e) {
+			e.printStackTrace();
 			System.out.println("[소분류 실적시작일 입력 오류]");
 		}
 		
